273cbd10073a53df116039d80ccfdeb25643a32f
Milena Mathew <mathewmilena@gmail.com>
2021-08-04 12:10:54 -0700

    test script

--- examples/scripts/test_ccpo.py
+++ examples/scripts/test_ccpo.py
@@ -20,18 +20,19 @@ def actions(index: int, context: Context) -> Sequence[Action]:
     #return [ tuple(a/sum(action) for a in action) for action in actions ]
 
 def rewards(index: int, context: Context, action: Action) -> float:
-    return sum(c*a for c,a in zip(context,action))
-
-def feedback(index: int, context: Context, action: Action) -> float:
-    print(action)
     if action == 1:
-        return tuple((0.5, -0.2))
+        return 0.5
+    else:
+        return 0.9
+
+def feedback(index: int, context: Context, action: Action) -> Sequence[float]:
+    if action == 1:
+        return tuple((0.5, -1))
     return tuple((0.9, 0.8))
 
 sim = [ConstrainedSimulation(n_interactions, context, actions, feedback)]
 
 Benchmark(sim).evaluate([ChanceConstrainedOptimizer(constraint=0.1, len_feedback=1, learning_rate=0.3, learner=VowpalLearner, vw_kwargs={"bag":5, "seed":10}), ChanceConstrainedOptimizer(constraint=0.1, len_feedback=1, learning_rate=0.3, learner=RandomLearner)]).plot_learners()
 
-#ref_sim = [LambdaSimulation(n_interactions, context, actions, rewards)]
-
-#Benchmark(ref_sim).evaluate([VowpalLearner(constraint=0.1, learning_rate=0.3, vw_kwargs={"bag":5, "seed":10})]).plot_learners()
+ref_sim = [LambdaSimulation(n_interactions, context, actions, rewards)]
+Benchmark(ref_sim).evaluate([VowpalLearner(constraint=0.1, learning_rate=0.3, vw_kwargs={"bag":5, "seed":10})]).plot_learners()
